2eda4410d68c67a06f04e67d550e749440ccc2d4
Nicholas Michalski <Nicholas.A.Michalski@gmail.com>
2013-08-07 16:45:37 -0500

    renamed and consolidated methods to be more meaningful

--- ruby/youtube/youtube_search.rb
+++ ruby/youtube/youtube_search.rb
@@ -7,40 +7,30 @@ require 'json'
 #
 class YoutubeSearch
   class << self
-    def search_youtube_for(user_input)
-      query_string = encode_for_url(user_input)
+    def search_youtube_for(query_string)
       search_url = generate_search_url(query_string)
       response = make_request_to_url(search_url)
-      urls = parse_response(response)
+      urls = extract_urls_from_response(response)
     end
 
     private
 
-    def encode_for_url(plain_input)
-      URI::encode(plain_input)
-    end
-
     def generate_search_url(query_string)
-      "http://gdata.youtube.com/feeds/api/videos?max-results=3&alt=json&q=#{query_string}"
+      encoded_query_string = URI::encode(query_string)
+      "http://gdata.youtube.com/feeds/api/videos?max-results=3&alt=json&q=#{encoded_query_string}"
     end
 
     def make_request_to_url(search_url)
       open(search_url).read
     end
 
-    def parse_response(response)
-      urls = []
-
+    def extract_urls_from_response(response)
       results = JSON.parse(response)
-      results["feed"]["entry"].each do |result|
-        urls << result_url(result)
-      end
-
-      urls
+      results["feed"]["entry"].collect{|result| result_url(result)}
     end
 
     def result_url(result)
-      result["link"].first["href"].gsub('&feature=youtube_gdata', '')
+      result["link"].first["href"]
     end
   end
 end
